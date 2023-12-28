97cf0b3a8c0b59d6a06c13411696438f101e6f0f
sonhj0125 <154120089+sonhj0125@users.noreply.github.com>
2023-12-28 15:16:03 +0900

    23.12.28

---
+++ Hello/자바의정석기초편/chap2/_03_예제2_5.java
@@ -0,0 +1,15 @@
+package chap2;
+
+public class _03_예제2_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 5;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+	}
+
+}
