bdc23517b44744b8e715fbc807c19c8b4e68bf92
sonhj0125 <154120089+sonhj0125@users.noreply.github.com>
2023-12-30 16:33:09 +0900

    23.12.30

---
+++ Hello/자바의정석기초편/chap2/_04_예제2_6.java
@@ -0,0 +1,17 @@
+package chap2;
+
+public class _04_예제2_6 {
+
+	public static void main(String[] args) {
+		int x = 100;
+		double pi = 3.14;
+		char ch = 'a';
+		String str = "abc";
+		
+		System.out.println(x);
+		System.out.println(pi);
+		System.out.println(ch);
+		System.out.println(str);
+	}
+
+}
