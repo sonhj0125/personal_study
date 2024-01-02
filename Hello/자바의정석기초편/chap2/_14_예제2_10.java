4b8f7170f8152c43a8c9627f80e7168d77ee4014
sonhj0125 <154120089+sonhj0125@users.noreply.github.com>
2024-01-02 16:36:50 +0900

    24.01.02

---
+++ Hello/자바의정석기초편/chap2/_14_예제2_10.java
@@ -0,0 +1,18 @@
+package chap2;
+
+import java.util.Scanner;                          // Scanner클래스를 사용하기 위해 추가
+
+public class _14_예제2_10 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);              // Scanner클래스의 객체를 생성
+		
+		System.out.print("두자리 정수를 하나 입력해주세요. > ");
+		String input = scanner.nextLine();                       // 입력받은 내용을 input에 저장
+		int num = Integer.parseInt(input);                      // 입력받은 문자열을 숫자로 변환
+		
+		System.out.println("입력내용 : "+input);
+		System.out.printf("num = %d%n",  num);
+	}
+
+}
